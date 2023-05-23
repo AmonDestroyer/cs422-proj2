b12b00868478da3f4d048c272e286de410e2c1d6
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-05-23 11:02:48 -0700

    add Keyword, Course, Major, OfferTimes, and Forecast models, as well as bridge tables for those models (in ManyToManyFields in model attributes)

--- cs_degree_planner/forecast/models.py
+++ cs_degree_planner/forecast/models.py
@@ -1,3 +1,83 @@
+"""
+TODO: file description
+
+2023-05-23 - Zane Globus-O'Harra : Add Keyword, Course, Major, and
+    OfferTimes models. The order that the models are written in matters.
+    The Forecast model is defined in users.models to avoid circular
+    imports. 
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Keyword(models.Model):
+    """
+    A Keyword is a word that is related to a class. For example, CS 422,
+    Software Methodologies I, might have the keywords "software", 
+    "software design", "group projects", etc. A user can select the
+    keywords that interest them so that the forecasting algorithm can 
+    tailor a schedule to their interests.
+    """
+    # attributes
+    kw_name = models.CharField(max_length=100)
+    kw_desc = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.kw_name
+
+
+class Course(models.Model):
+    # TODO: docstring
+    """
+    """
+    # attributes
+    course_name = models.CharField(max_length=100)
+    credits = models.IntegerField()
+
+    # bridge table to get course prereqs
+    has_prereq = models.ManyToManyField("self")
+    is_prereq_for = models.ManyToManyField("self")
+
+    # bridge table to get course keywords
+    has_kw = models.ManyToManyField(Keyword)
+
+    def __str__(self):
+        return self.course_name
+
+
+class Major(models.Model):
+    # TODO: docstring
+    """
+    """
+    # attributes
+    major_name = models.CharField(max_length=100)
+    major_desc = models.CharField(max_length=512)
+    is_minor = models.BooleanField(default=False)
+
+    # bridge table to get the courses in a major
+    has_course = models.ManyToManyField(Course)
+    
+    def __str__(self):
+        return self.name 
+
+
+class OfferTimes(models.Model):
+    # TODO: docstring
+    """
+    """
+    # the offer times are identified by the course that is offered
+    course = models.OneToOneField(
+        Course,
+        on_delete=models.CASCADE
+    )
+    # attributes
+    fall = models.BooleanField(default=False);
+    winter = models.BooleanField(default=False);
+    spring = models.BooleanField(default=False);
+    summer = models.BooleanField(default=False);
+    every_year = models.BooleanField(default=False);
+    every_year_odd = models.BooleanField(default=False);
+    every_year_even = models.BooleanField(default=False);
+
+    def __str__(self):
+        return self.course
--- cs_degree_planner/users/models.py
+++ cs_degree_planner/users/models.py
@@ -5,11 +5,14 @@ TODO: file description
     get the User model. For in-depth look at the User model's fields and 
     methods, see:
     https://docs.djangoproject.com/en/4.2/ref/contrib/auth/#user-model
-
+2023-05-23 - Zane Globus-O'Harra : add Forecast model, and imports from 
+    forecast.models. The Forecast model needed to be defined here to 
+    avoid circular imports. 
 """
 
 from django.db import models
 from django.contrib.auth.models import User
+from forecast.models import Major, Course, Keyword
 
 
 class Profile(models.Model):
@@ -25,6 +28,27 @@ class Profile(models.Model):
         User, 
         on_delete=models.CASCADE
     )
+    major = models.ManyToManyField(Major)           # bridge table to majors
+    courses_taken = models.ManyToManyField(Course)  # bridge table to courses
+    interests = models.ManyToManyField(Keyword)     # bridge table to keywords
 
     def __str__(self):
         return self.user.username
+
+
+class Forecast(models.Model):
+    """
+    Forecast model class. Each forecast is associated with a user such 
+    that a user can have multiple forecasts that they have generated. 
+
+    This model needs to be in users.models because if it were in 
+    forecast.models, there would be circular imports. 
+    """
+    courses_in_fc = models.ManyToManyField(Course)  # bridge table to courses
+    user = models.ForeignKey(                       # related to a user profile
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user
