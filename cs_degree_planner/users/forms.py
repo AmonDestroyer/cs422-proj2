bcfef5d9697ccb638dead035136de65d6de62603
fleeber <erinliannestone@gmail.com>
2023-05-25 14:12:56 -0700

    some changes forms.py for styling, lots of changes to create_user.html for styling
    some changes forms.py for styling, lots of changes to create_user.html for styling

--- cs_degree_planner/users/forms.py
+++ cs_degree_planner/users/forms.py
@@ -3,6 +3,7 @@ TODO: file description
 
 2023-05-19 - Nathaniel mason : add JEANZUserCreationForm
 2023-05-22 - Josh Sawyer     : add JEANZUserLoginForm
+2023-05-25 - Erin Stone      : minor changes to usercreationform for styling
 
 """
 
@@ -18,12 +19,12 @@ from django.contrib.auth.models import User
 # UserCreationForm is a ModelForm class that has password validation and can be saved
 # Will create a custom version of UserCreationForm to add some other fields
 class JEANZUserCreationForm(UserCreationForm):
-    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'id': 'username', 'placeholder': 'Enter Username'}), max_length=50, help_text="50 characters max and can contain letters, digits and @/./+/-/_")
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'id': 'email', 'placeholder': 'Email'}), max_length=254, help_text="254 characters max")
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'id': 'password1', 'placeholder': 'Enter Password'}), min_length=8, help_text="At least 8 characters")
-    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={'id': 'password2', 'placeholder': 'Enter Password Again'}), help_text="Enter same password to confirm") # 2nd password used to check and match the 1st one
-    first_name = forms.CharField(label="First Name", widget=forms.TextInput(attrs={'id': 'first_name', 'placeholder': 'Enter First Name'}), max_length=50, help_text="50 characters max")
-    last_name = forms.CharField(label="Last Name", widget=forms.TextInput(attrs={'id': 'last_name', 'placeholder': 'Enter Last Name'}), max_length=50, help_text="50 characters max")
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'fld', 'id': 'username', 'placeholder': 'USERNAME', 'autofocus': 'True'}), max_length=50, help_text="50 characters max and can contain letters, digits and @/./+/-/_")
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'fld', 'id': 'email', 'placeholder': 'EMAIL ADDRESS'}), max_length=254, help_text="254 characters max")
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'fld', 'id': 'password1', 'placeholder': 'PASSWORD'}), min_length=8, help_text="At least 8 characters")
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={'class': 'fld', 'id': 'password2', 'placeholder': 'CONFIRM PASSWORD'}), help_text="Enter same password to confirm") # 2nd password used to check and match the 1st one
+    first_name = forms.CharField(label="First Name", widget=forms.TextInput(attrs={'class': 'fld', 'id': 'first_name', 'placeholder': 'FIRST NAME'}), max_length=50, help_text="50 characters max")
+    last_name = forms.CharField(label="Last Name", widget=forms.TextInput(attrs={'class': 'fld', 'id': 'last_name', 'placeholder': 'LAST NAME'}), max_length=50, help_text="50 characters max")
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -36,7 +37,7 @@ class JEANZUserCreationForm(UserCreationForm):
         username = self.cleaned_data['username'].lower()
         new = User.objects.filter(username = username)
         if new.count():
-            raise ValidationError("User Already Used")
+            raise ValidationError("Username already taken")
         return username
     
     def clean_email(self):
@@ -44,7 +45,7 @@ class JEANZUserCreationForm(UserCreationForm):
         email = self.cleaned_data['email'].lower()
         new = User.objects.filter(email=email)
         if new.count():
-            raise ValidationError("Email Already Used")
+            raise ValidationError("Email already taken")
         return email
     
     def clean_password2(self):
@@ -53,7 +54,7 @@ class JEANZUserCreationForm(UserCreationForm):
         password2 = self.cleaned_data['password2']
 
         if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords Do Not Match")
+            raise ValidationError("Passwords do not match")
         return password2
     
     # first_name and last_name could theoretically be the same for multiple users, so don't need to check if already used
