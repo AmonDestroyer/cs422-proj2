f2652f546017bd5bdf06cce7cb978c67d6504b70
Nathaniel Mason <nmason0204@gmail.com>
2023-05-24 19:15:46 -0700

    Updated Edit Courses template so that sends data to Django
    note: still needs to be updated to save the course/credit info in the DB, currently Django collects the data and then displays it back to the user using Django messages

---
+++ cs_degree_planner/forecast/forms.py
@@ -0,0 +1,46 @@
+"""
+TODO: file description
+
+2023-05-24 - Nathaniel mason : add EditCoursesForm and code to pull course options
+
+"""
+
+import os
+import pandas as pd
+from django import forms
+
+current_dir = os.path.dirname(__file__)
+file_path = current_dir + '/recommendcourses.xlsx'
+
+df = pd.read_excel(file_path, index_col="id")
+
+COURSE_OPTIONS = []
+
+for index, row in df.iterrows():
+    required = row['required']
+    if(required):
+        list_option_display = row['subject'] + ' ' + str(row['number']) + ' ' + row['name']
+        list_option_val = str(index)
+        #print(list_option)
+        list_option = (list_option_val, list_option_display)
+        COURSE_OPTIONS.append(list_option)
+
+#print(COURSE_OPTIONS)
+
+# create multiple select Django form using the array of options
+class EditCoursesForm(forms.Form):
+    major_courses = forms.MultipleChoiceField(
+        label='', choices=COURSE_OPTIONS, widget=forms.SelectMultiple(
+            attrs={'id': 'course_select','class': 'chzn-select'}))
+    
+    sci_cred = forms.IntegerField(label='', widget=forms.NumberInput(
+        attrs={'id': 'science_cred', 'class': 'input-number', 
+                'type': 'number', 'value': '0', 'min': '0', 'max': '10'}))
+    
+    soc_sci_cred = forms.IntegerField(label='', widget=forms.NumberInput(
+        attrs={'id': 'social_science_cred', 'class': 'input-number', 
+               'type': 'number', 'value': '0', 'min': '0', 'max': '10'}))
+    
+    arts_letters_cred = forms.IntegerField(label='', widget=forms.NumberInput(
+        attrs={'id': 'arts_letters_cred', 'class': 'input-number', 
+               'type': 'number', 'value': '0', 'min': '0', 'max': '10'}))
--- cs_degree_planner/forecast/views.py
+++ cs_degree_planner/forecast/views.py
@@ -3,10 +3,13 @@ TODO: file description
 
 2023-05-22 - Josh Sawyer     : add dashboard view
 2023-05-23 - Nathaniel mason : add edit_courses view and courses_left view
+2023-05-24 - Nathaniel mason : update edit_courses view
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import EditCoursesForm
+from django.contrib import messages
 
 # login is required to see the dashboard
 # if user is not logged in, redirect them to the index/landing page 
@@ -16,7 +19,41 @@ def index(request):
 
 
 def edit_courses(request):
-    return render(request, "forecast/edit_courses.html")
+    if request.method != 'POST':
+        form = EditCoursesForm()
+        # user is just requesting the page
+        # just display the form for them to choose options from
+    
+    else:
+        form = EditCoursesForm(request.POST)
+        if form.is_valid():
+            # once the form is valid, we must save their chosen courses in the DB
+            user_courses_taken = form.cleaned_data.get('major_courses')
+            user_sci = form.cleaned_data.get('sci_cred')
+            user_soc_sci = form.cleaned_data.get('soc_sci_cred')
+            user_arts_lett = form.cleaned_data.get('arts_letters_cred')
+            
+            # next will need to take the user_courses_taken and save them in the DB for that user
+            # for now, just send messages back to notify that Django obtained the data correctly
+            messages.info(request, user_courses_taken)
+            messages.info(request, user_sci)
+            messages.info(request, user_soc_sci)
+            messages.info(request, user_arts_lett)
+
+            messages.success(request, "Changes Saved Successfully!")
+            
+            return redirect('forecast:edit_courses')
+        else:
+            for field in form:
+                if field.errors:
+                    print(field.errors) 
+            messages.error(request, "Error While Attempting to Save Changes")
+            
+            return redirect('forecast:edit_courses')
+
+    context = {'courseform': form}
+    
+    return render(request, "forecast/edit_courses.html", context)
 
 
 def courses_left(request):
